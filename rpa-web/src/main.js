36abcc6f90647e2293361dad1e2e5ecf8530e40e
Leego <i@leego.io>
2024-05-31 23:59:18 +0800

    11

--- rpa-web/src/main.js
+++ rpa-web/src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import i18n from "./i18n"
 import vuetify from "./vuetify"
+import toast from '@/components/Toast';
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import 'vue3-toastify/dist/index.css';
 
 createApp(App)
   .use(router)
