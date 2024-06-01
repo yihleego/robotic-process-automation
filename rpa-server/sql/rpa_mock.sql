d495dbb89ba1a9cb9b9ae59e9ed5d768976967da
Leego <leegodev@hotmail.com>
2024-06-02 04:31:31 +0800

    Polish

---
+++ rpa-server/sql/rpa_mock.sql
@@ -0,0 +1,7 @@
+# MySQL
+INSERT INTO user (app_id, account, nickname, realname, company, phone, avatar, status, created_time)
+VALUES ('wechat', 'leego', 'Leego', null, null, null, 'https://avatars.githubusercontent.com/u/21337386?v=4', 1, now()),
+       ('wecom', 'leego', 'Leego', 'Leego Yih', 'Spiral Studio', null, 'https://avatars.githubusercontent.com/u/21337386?v=4', 1, now()),
+       ('qq', 'leego', 'Leego', null, null, null, 'https://avatars.githubusercontent.com/u/21337386?v=4', 1, now()),
+       ('dingtalk', 'leego', 'Leego', null, null, null, 'https://avatars.githubusercontent.com/u/21337386?v=4', 1, now()),
+       ('lark', 'leego', 'Leego', null, null, null, 'https://avatars.githubusercontent.com/u/21337386?v=4', 1, now());
