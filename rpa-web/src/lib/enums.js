64d510bf7aa0b17614c38dc2bb9eeea92fa06bb7
Leego <i@leego.io>
2024-06-02 03:07:13 +0800

    11

--- rpa-web/src/lib/enums.js
+++ rpa-web/src/lib/enums.js
@@ -1,16 +1,2 @@
 export default {
-  task: {
-    status: {
-      0: "task.status.created",
-      1: "task.status.running",
-      2: "task.status.deleted",
-      3: "task.status.cancelled",
-      10: "task.status.finished",
-      11: "task.status.failed",
-      12: "task.status.timeout",
-      13: "task.status.offline",
-      14: "task.status.terminated",
-      15: "task.status.unsupported",
-    }
-  }
 }
